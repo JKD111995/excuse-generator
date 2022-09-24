ab4c7b65f8e48b298dcf6b8f93c9ef24a1cb3a18
JKD111995 <jake.duque@gmail.com>
2022-09-24 00:57:51 +0000

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function wordPicker(array) {
+  return "sdsd";
+}
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let result = "";
+  document.getElementById("excuse").innerHTML = result;
 };
